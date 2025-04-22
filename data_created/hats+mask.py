1bcf41bba9b42f9e4510397852f72d4313e77e4c
ashish <ashishkr5443dsa@gmail.com>
2025-04-22 12:56:16 +0530

    update the images position in hats+mask.py file

--- data_created/hats+mask.py
+++ data_created/hats+mask.py
@@ -18,31 +18,29 @@ os.makedirs(output_hat_mask, exist_ok=True)
 # Offsets
 accessory_offsets = {
     'hat1.png': (5.4, -10),
-    'hat2.png': (4.8, -55),
-    'hat3.png': (5.6, -30),
-    'hat4.png': (5.8, -26),
-    'hat5.png': (10, -50),
-    'hat6.png': (10, -11),
+    'hat2.png': (4.8, -52),
+    'hat3.png': (5.2, -25),
+    'hat4.png': (6.6, -26),
+    'hat5.png': (17, -50),
+    'hat6.png': (9, -11),
     'hat7.png': (6, -12),
-    'hat8.png': (4, -16),
-    'hat9.png': (3, -35),
-    'hat10.png': (8.5, -16),
-    'hat11.png': (5.3, -17.7),
-    'hat12.png': (3, -30),
-    'hat13.png': (5, -25),
-    'hat14.png': (4.2, -20),
-    'hat15.png': (4.5, -40),
-    'hat16.png': (4.2, -25),
-    'hat17.png': (1, -40),
-    'hat18.png': (5, -36),
-    'hat19.png': (3, -37),
-    'hat20.png': (5, -30),
-
-    # mask offsets 
-    'mask1.png': (0, 0),
+    'hat8.png': (3, -50),
+    'hat9.png': (1, -45),
+    'hat10.png': (8.5, -20),
+    'hat11.png': (5.3, -22),
+    'hat12.png': (5.6, -45),
+    'hat13.png': (4.5, -28),
+    'hat14.png': (5.5, -12),
+    'hat15.png': (4.2, -40),
+    'hat16.png': (5.4, -30),
+    'hat17.png': (1.6, -30),
+    'hat18.png': (5, -48),
+    'hat19.png': (2.5, -42),
+    'hat20.png': (5.7, -32),
+    # 'mask1.png': (0, 0),
     'mask2.png': (0, 0),
-    'mask3.png': (0, 0),
-    'mask4.png': (0, 0),
+    # 'mask3.png': (0, 0),
+    # 'mask4.png': (0, 0),
 }
 
 def get_landmarks(image):
@@ -74,12 +72,12 @@ def place_hat_mask(image_path, hat_name, mask_name, index):
     hat_path = os.path.join(hat_folder, hat_name)
     hat_img = Image.open(hat_path)
     hat_offset_x, hat_offset_y = accessory_offsets.get(hat_name, (7, -35))
-    hat_w = int(face_width * 1.5)
+    hat_w = int(face_width * 1.6)
     hat_h = int(face_height * 1.52)
     hat_x = int(forehead[0] - hat_w // 2 + hat_offset_x)
     hat_y = int(forehead[1] - hat_h + hat_offset_y)
     face_img = overlay_accessory(face_img, hat_img, hat_x, hat_y, hat_w, hat_h)
-
+    
     # Mask
     mask_path = os.path.join(mask_folder, mask_name)
     mask_img = Image.open(mask_path)
@@ -89,8 +87,8 @@ def place_hat_mask(image_path, hat_name, mask_name, index):
     jaw_left = landmarks[3]
     jaw_right = landmarks[13]
     mask_center = nose
-    mask_w = int(np.linalg.norm(jaw_right - jaw_left) * 1.3)
-    mask_h = int(face_height * 1)
+    mask_w = int(np.linalg.norm(jaw_right - jaw_left) * 2)
+    mask_h = int(face_height * 1.8)
 
     mask_x = int(mask_center[0] - mask_w // 2 + mask_offset_x)
     mask_y = int(mask_center[1] - mask_h // 3 + mask_offset_y)
@@ -101,14 +99,14 @@ def place_hat_mask(image_path, hat_name, mask_name, index):
     face_img.convert('RGB').save(os.path.join(output_hat_mask, filename))
     print(f"save_img: {filename}")
 
-# Image and accessory lists
+# Get images in serial order
 hat_list = [f'hat{i}.png' for i in range(1, 21)]
-mask_list = [f'mask{i}.png' for i in range(1, 5)]
+mask_list = [f'mask{i}.png' for i in range(1,6)]
 
 # Sorted input image list
 input_files = sorted([f for f in os.listdir(input_folder) if f.lower().endswith(('.jpg', '.jpeg', '.png'))])
 
-# Apply combination
+# fixed pairs
 for i, file in enumerate(input_files):
     hat_name = hat_list[i % len(hat_list)]
     mask_name = mask_list[i % len(mask_list)]
